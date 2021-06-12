fd6fa0a7f91432b97a8b2edf593c324acdc8be4b
starsli <starsli@tencent.com>
2021-06-12 17:06:15 +0800

    不可以复制类，屏蔽了拷贝构造&赋值运算符重载

--- muduo/base/noncopyable.h
+++ muduo/base/noncopyable.h
@@ -3,18 +3,29 @@
 
 namespace muduo
 {
-
+// 不可复制类
 class noncopyable
 {
  public:
-  noncopyable(const noncopyable&) = delete;
-  void operator=(const noncopyable&) = delete;
+  noncopyable(const noncopyable&) = delete;  // 禁用拷贝构造
+  void operator=(const noncopyable&) = delete;  // 禁用赋值操作符重载
 
  protected:
-  noncopyable() = default;
-  ~noncopyable() = default;
+  noncopyable() = default;  // 使用默认的构造函数
+  ~noncopyable() = default;  // 使用默认的析构函数
 };
 
 }  // namespace muduo
 
 #endif  // MUDUO_BASE_NONCOPYABLE_H
+
+// 类中默认的六个成员函数
+// 初始化&清理
+// 1）构造函数
+// 2）析构函数
+// 拷贝&赋值
+// 3）拷贝构造
+// 4）赋值运算符重载
+// 取地址重载
+// 5）取地址符重载
+// 6）const取地址符重载
